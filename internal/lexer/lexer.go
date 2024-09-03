239314e8884bb8875e1d728d5119f9eccc43daad
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-09-03 15:05:04 -0300

    refactor: modularizing lexer

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,2 +1,135 @@
 package lexer
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/dinizgab/toy_compiler/internal/token"
+	"github.com/dinizgab/toy_compiler/internal/utils"
+)
+
+
+type Lexer struct {
+	Input   string
+	Cursor  int
+	Forward int
+}
+
+func (l *Lexer) Lex() ([]*token.Token, error) {
+	tokens := make([]*token.Token, 0)
+
+	for l.Cursor < len(l.Input) {
+		char := l.peek()
+
+		switch {
+			case utils.IsAlpha(char):
+				token, err := l.readIdentifier()
+				if err != nil {
+					return nil, err
+				}
+				tokens = append(tokens, token)
+			case utils.IsNum(char):
+				token, err := l.readNum()
+				if err != nil {
+					return nil, err
+				}
+				tokens = append(tokens, token)
+			case utils.IsOperator(char):
+				token, err := l.readOperator()
+				if err != nil {
+					return nil, err
+				}
+				tokens = append(tokens, token)
+			case utils.IsBracket(char):
+				token, err := token.NewToken(string(char), string(char))
+				if err != nil {
+					return nil, err
+				}
+				tokens = append(tokens, token)
+			case char == 10 || char == 32:
+				// ignoring new lines and spaces
+			default:
+				return nil, fmt.Errorf("lexer.Lex: Invalid character: %s", string(char))
+		}
+
+		l.nextLexeme()
+	}
+
+	return tokens, nil
+}
+
+func (l *Lexer) peek() byte {
+	return l.Input[l.Cursor]
+}
+
+func (l *Lexer) peekNextChar() byte {
+	return l.Input[l.Cursor+1]
+}
+
+func (l *Lexer) nextLexeme() {
+	l.Cursor = l.Forward
+	l.Forward += 1
+}
+
+
+func (l *Lexer) readIdentifier() (*token.Token, error) {
+	for l.Forward < len(l.Input) && (utils.IsAlpha(l.Input[l.Forward]) || utils.IsNum(l.Input[l.Forward])) {
+		l.Forward++
+	}
+
+	lexeme := l.Input[l.Cursor:l.Forward]
+	// If lexeme is a keyword
+	if token.IsKeyword(lexeme) {
+		return token.NewToken(lexeme, lexeme)
+	}
+
+	return token.NewToken(token.TokenIdent, lexeme)
+}
+
+func (l *Lexer) readNum() (*token.Token, error) {
+	dotCounter := 0
+	for l.Forward < len(l.Input) && utils.IsNum(l.Input[l.Forward]) || l.Input[l.Forward] == '.' {
+		if l.Input[l.Forward] == '.' {
+			dotCounter++
+
+			if dotCounter > 1 {
+				return nil, errors.New("lexer.readNum: Number with more than one '.'")
+			}
+		}
+
+		l.Forward++
+	}
+
+	lexeme := l.Input[l.Cursor:l.Forward]
+
+	token, err := token.NewToken(token.TokenNumber, lexeme)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func (l *Lexer) readOperator() (*token.Token, error) {
+	if utils.IsOperator(l.peekNextChar()) {
+		l.Forward++
+	}
+
+	var tokenType string
+	lexeme := l.Input[l.Cursor:l.Forward]
+
+	switch lexeme {
+	case "==", "!=", "&&", "||", ">", "<", "<=", ">=", "!":
+		tokenType = token.TokenLogicalOperator
+	case "=":
+		tokenType = token.TokenAssign
+	default:
+		tokenType = token.TokenArithmeticOperator
+	}
+
+	token, err := token.NewToken(tokenType, lexeme)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
