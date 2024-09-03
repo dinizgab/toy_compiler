d2fd4e8b838379d433241b4b7c6317d54748d3b3
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-09-03 13:48:14 -0300

    refactor: using const variables to specifi tokens

--- internal/lexer/token.go
+++ internal/lexer/token.go
@@ -2,21 +2,38 @@ package lexer
 
 import "fmt"
 
+const (
+    TokenIf                 = "IF"
+    TokenElseIf             = "ELSE_IF"
+    TokenElse               = "ELSE"
+    TokenFn                 = "FN"
+    TokenReturn             = "RETURN"
+    TokenOpenParen          = "OPEN_PAREN"
+    TokenCloseParen         = "CLOSE_PAREN"
+    TokenOpenBrack          = "OPEN_BRACK"
+    TokenCloseBrack         = "CLOSE_BRACK"
+    TokenIdent              = "IDENT"
+    TokenNumber             = "NUMBER"
+    TokenLogicalOperator    = "LOG_OPERATOR"
+    TokenArithmeticOperator = "ARITH_OPERATOR"
+    TokenAssign             = "ASSIGN"
+)
+
 var tokens = map[string]string{
-	"if":                 "IF",
-	"else if":            "ELSE_IF",
-	"else":               "ELSE",
-	"fn":                 "FN",
-	"return":             "RETURN",
-	"(":                  "OPEN_PAREN",
-	")":                  "CLOSE_PAREN",
-	"{":                  "OPEN_BRACK",
-	"}":                  "CLOSE_BRACK",
-	"id":                 "IDENT",
-	"number":             "NUMBER",
-	"logicalOperator":    "LOG_OPERATOR",
-	"arithmeticOperator": "ARITH_OPERATOR",
-	"assign":             "ASSIGN",
+    "if":                 TokenIf,
+    "else if":            TokenElseIf,
+    "else":               TokenElse,
+    "fn":                 TokenFn,
+    "return":             TokenReturn,
+    "(":                  TokenOpenParen,
+    ")":                  TokenCloseParen,
+    "{":                  TokenOpenBrack,
+    "}":                  TokenCloseBrack,
+    "id":                 TokenIdent,
+    "number":             TokenNumber,
+    "logicalOperator":    TokenLogicalOperator,
+    "arithmeticOperator": TokenArithmeticOperator,
+    "assign":             TokenAssign,
 }
 
 type Token struct {
